46f9e0426026abe77c9216fc0fea3071e1043664
Yotam Perlitz <yotam.perlitz@ibm.com>
2024-07-18 11:09:29 +0300

    align to leaderboard

--- src/bat/__init__.py
+++ src/bat/__init__.py
@@ -3,3 +3,9 @@
 __author__ = """Yotam Perlitz"""
 __email__ = "yotam.perlitz@ibm.com"
 __version__ = "0.1.0"
+
+
+from bat.agreement_tester import Tester
+from bat.benchmark import Benchmark
+from bat.configs import Config
+from bat.reporting import Reporter
